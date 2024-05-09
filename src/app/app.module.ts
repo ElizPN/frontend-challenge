f73357c195503c0d07e1e9d2bcefcf3f5e8245b5
ElizPN <pankevych.liza@gmail.com>
2024-05-09 16:49:46 +0200

    feat: add initial styles and text function

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms'; // Import FormsModule
+import { AppComponent } from './app.component';
+import { CountdownComponent } from './countdown/countdown.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CountdownComponent
+  ],
+
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: []
+})
+export class AppModule { }
--- src/app/countdown/countdown.component.ts
+++ src/app/countdown/countdown.component.ts
@@ -2,11 +2,16 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-countdown',
-  standalone: true,
-  imports: [],
   templateUrl: './countdown.component.html',
   styleUrl: './countdown.component.scss'
 })
 export class CountdownComponent {
+  inputValue: string = ''
+
+  constructor() {}
+
+  logTextToConsole(text: string) {
+    console.log(this.inputValue);
+  }
 
 }
