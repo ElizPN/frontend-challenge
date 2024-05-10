13711fea0073504bb0699e92c0bf4106c7b067c7
ElizPN <pankevych.liza@gmail.com>
2024-05-10 14:40:06 +0200

    feat: add dynamic countdown

--- src/app/countdown/countdown.component.ts
+++ src/app/countdown/countdown.component.ts
@@ -1,36 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { interval, Subscription } from 'rxjs'
 
 @Component({
   selector: 'app-countdown',
   templateUrl: './countdown.component.html',
-  styleUrl: './countdown.component.scss'
+  styleUrls: ['./countdown.component.scss'],
 })
-export class CountdownComponent {
-  inputValue: string = '';
-  timeDifference: string = '';
+export class CountdownComponent implements OnInit {
+  inputValue: string = ''
+  timeDifference: { days: number; hours: number; minutes: number; seconds: number } = {
+    days: 0,
+    hours: 0,
+    minutes: 0,
+    seconds: 0,
+  }
+  private timerSubscription: Subscription | undefined
 
   constructor() {}
 
-  calculateTimeDifference() {
-    const currentDate = new Date();
-    const selectedDate = new Date(this.inputValue);
+  ngOnInit() {
+    this.startTimer()
+  }
 
-    const timeDiff = this.getTimeDifference(selectedDate, currentDate);
-    this.timeDifference = `${timeDiff.days} days, ${timeDiff.hours} hours, ${timeDiff.minutes} minutes, ${timeDiff.seconds} seconds`;
+  private startTimer() {
+    this.timerSubscription = interval(1000).subscribe(() => {
+      this.calculateTimeDifference()
+    })
+  }
+
+
+  calculateTimeDifference() {
+    const currentDate = new Date()
+    const selectedDate = new Date(this.inputValue)
+
+    this.timeDifference = this.getTimeDifference(selectedDate, currentDate)
   }
 
   private getTimeDifference(selectedDate: Date, currentDate: Date) {
-    const differenceInMilliseconds = Math.abs(selectedDate.getTime() - currentDate.getTime());
-    const seconds = Math.floor(differenceInMilliseconds / 1000);
-    const minutes = Math.floor(seconds / 60);
-    const hours = Math.floor(minutes / 60);
-    const days = Math.floor(hours / 24);
+    const differenceInMilliseconds = Math.abs(selectedDate.getTime() - currentDate.getTime())
+    const seconds = Math.floor(differenceInMilliseconds / 1000)
+    const minutes = Math.floor(seconds / 60)
+    const hours = Math.floor(minutes / 60)
+    const days = Math.floor(hours / 24)
 
     return {
       days: days % 365,
       hours: hours % 24,
       minutes: minutes % 60,
-      seconds: seconds % 60
-    };
+      seconds: seconds % 60,
+    }
   }
 }
