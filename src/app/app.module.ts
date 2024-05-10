c8e6232059d61c4c3c65e8d96d2a942b56b70239
ElizPN <pankevych.liza@gmail.com>
2024-05-10 10:14:36 +0200

    fix: remove standalone component from declaration; replace browser module with common module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,19 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms'; // Import FormsModule
-import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 import { CountdownComponent } from './countdown/countdown.component';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
-    AppComponent,
+    CountdownComponent
+  ],
+  exports: [
     CountdownComponent
   ],
 
   imports: [
-    BrowserModule,
-    FormsModule
+    FormsModule,
+    CommonModule,
   ],
   providers: [],
   bootstrap: []
