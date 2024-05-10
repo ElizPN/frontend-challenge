eeaf5acdcfe491e919e1066173e4ecaf0e349381
ElizPN <pankevych.liza@gmail.com>
2024-05-10 14:03:48 +0200

    feat: add time static differense

--- src/app/countdown/countdown.component.ts
+++ src/app/countdown/countdown.component.ts
@@ -6,12 +6,31 @@ import { Component } from '@angular/core';
   styleUrl: './countdown.component.scss'
 })
 export class CountdownComponent {
-  inputValue: string = ''
+  inputValue: string = '';
+  timeDifference: string = '';
 
   constructor() {}
 
-  logTextToConsole(text: string) {
-    console.log(this.inputValue);
+  calculateTimeDifference() {
+    const currentDate = new Date();
+    const selectedDate = new Date(this.inputValue);
+
+    const timeDiff = this.getTimeDifference(selectedDate, currentDate);
+    this.timeDifference = `${timeDiff.days} days, ${timeDiff.hours} hours, ${timeDiff.minutes} minutes, ${timeDiff.seconds} seconds`;
   }
 
+  private getTimeDifference(selectedDate: Date, currentDate: Date) {
+    const differenceInMilliseconds = Math.abs(selectedDate.getTime() - currentDate.getTime());
+    const seconds = Math.floor(differenceInMilliseconds / 1000);
+    const minutes = Math.floor(seconds / 60);
+    const hours = Math.floor(minutes / 60);
+    const days = Math.floor(hours / 24);
+
+    return {
+      days: days % 365,
+      hours: hours % 24,
+      minutes: minutes % 60,
+      seconds: seconds % 60
+    };
+  }
 }
